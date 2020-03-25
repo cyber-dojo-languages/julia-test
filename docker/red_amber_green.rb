9a498655f7b5cd0e313c85438208ef671d60b4cb
Jon Jagger <jon@jaggersoft.com>
2020-03-25 11:16:09 +0000

    Reset red_amber_green.rb as TODO

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,12 +1,5 @@
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
-  ruby_pattern = Regexp.new('^(\d+) runs, (\d+) assertions, (\d+) failures, (\d+) errors')
-  if match = ruby_pattern.match(output)
-    return :amber if match[4] != '0'
-    return :red   if match[3] != '0'
-    return :green
-  else
-    return :amber
-  end
+  return :amber # TODO
 }
