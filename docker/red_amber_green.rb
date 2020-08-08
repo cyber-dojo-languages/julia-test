838b4646f25e267116900dbfc4031f24d8f98de4
Jon Jagger <jon@jaggersoft.com>
2020-08-08 16:47:54 +0100

    Add simplest possible working red_amber_green.rb file

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,5 +1,6 @@
 
-lambda { |stdout,stderr,status|
-  output = stdout + stderr
-  return :amber # TODO
+lambda { |stdout,_stderr,status|
+  return :green if status === 0
+  return :red if stdout.include?('Test Failed at')
+  return :amber
 }
